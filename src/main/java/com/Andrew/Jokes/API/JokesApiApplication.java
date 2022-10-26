594029c13dcfc17cb1df77b862c735d5b3b70242
Godofallbacon <drewnaugle@comcast.net>
2022-10-26 09:36:46 -0400

    Finished API

---
+++ src/main/java/com/Andrew/Jokes/API/JokesApiApplication.java
@@ -0,0 +1,13 @@
+package com.Andrew.Jokes.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokesApiApplication.class, args);
+	}
+
+}
